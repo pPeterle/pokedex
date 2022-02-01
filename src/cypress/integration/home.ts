ab4d11ccd42f867bd596840fce89628305a05f0e
pPeterle <phpeterle1@gmail.com>
2022-02-01 12:45:47 -0300

    testes e2e refatorado

--- src/cypress/integration/home.ts
+++ src/cypress/integration/home.ts
@@ -1,51 +1,50 @@
+import { HomePage } from '../pages/home_page';
+
 describe('Home page', () => {
   const SEARCH_TERM = 'kabuto';
   const SEARCH_WRONG_TERM = 'abcdefs';
 
+  let homePage: HomePage;
+
   beforeEach(() => {
-    cy.visit('/');
+    homePage = new HomePage();
+    homePage.visit();
   });
 
   it('search the next page and previus page', () => {
-    cy.byTestId('pokemon-item').should('have.length', 20);
-    cy.byTestId('pokemon-item-id').first().should('have.text', '#001');
-    cy.byTestId('next-page-button').first().click();
+    homePage.getPokemonItems().should('have.length', 20);
+    homePage.getFirstPokemonItemId().should('have.text', '#001');
+    homePage.clickNextPageButton();
     cy.wait(5000);
-    cy.byTestId('pokemon-item-id').first().should('have.text', '#021');
-    cy.byTestId('previus-page-button').first().click();
+    homePage.getFirstPokemonItemId().first().should('have.text', '#021');
+    homePage.clickPreviusPageButton();
     cy.wait(1000);
-    cy.byTestId('pokemon-item-id').first().should('have.text', '#001');
+    homePage.getFirstPokemonItemId().first().should('have.text', '#001');
   });
 
   it('search by specific pokemon and clear input', () => {
-    cy.byTestId('search-pokemon-input').first().clear().type(SEARCH_TERM);
-
-    cy.byTestId('search-button').first().click();
+    homePage.searchForTerm(SEARCH_TERM);
     cy.wait(500);
-    cy.byTestId('pokemon-item').should('have.length', 1);
+    homePage.getPokemonItems().should('have.length', 1);
 
-    cy.byTestId('clear-button').first().click();
+    homePage.clickClearButton();
     cy.wait(500);
-    cy.byTestId('pokemon-item').should('have.length', 20);
+    homePage.getPokemonItems().should('have.length', 20);
   });
 
   it('show error when search pokemon that not exists', () => {
-    cy.byTestId('search-pokemon-input').first().clear().type(SEARCH_WRONG_TERM);
-    cy.byTestId('search-button').first().click();
+    homePage.searchForTerm(SEARCH_WRONG_TERM);
     cy.wait(500);
 
-    cy.get('snack-bar-container').first().should('be.visible');
-  })
+    homePage.getSnackBar().should('be.visible');
+  });
 
   it('select and batle pokemons', () => {
-    cy.byTestId('batle-pokemon-button')
-      .first()
-      .click()
-      .should('have.class', 'selected');
+    homePage.clickStartBatlePokemon().should('have.class', 'selected');
 
-    cy.byTestId('pokemon-item').eq(0).click().should('have.class', 'selected');
-    cy.byTestId('pokemon-item').eq(1).click().should('have.class', 'selected');
+    homePage.clickFirstPokemonItem().should('have.class', 'selected');
+    homePage.clickSecondPokemonItem().should('have.class', 'selected');
 
-    cy.byTestId('fight-pokemon-dialog-title').first().should('be.visible');
+    homePage.getTitleBatleDialog().should('be.visible');
   });
 });
---
+++ src/cypress/pages/home_page.ts
@@ -0,0 +1,50 @@
+export class HomePage {
+  public visit() {
+    cy.visit('/');
+  }
+
+  public getPokemonItems() {
+    return cy.byTestId('pokemon-item');
+  }
+
+  public getFirstPokemonItemId() {
+    return cy.byTestId('pokemon-item-id').first();
+  }
+
+  public clickNextPageButton() {
+    return cy.byTestId('next-page-button').first().click();
+  }
+
+  public clickClearButton() {
+    return cy.byTestId('clear-button').first().click();
+  }
+
+  public searchForTerm(term: string) {
+    cy.byTestId('search-pokemon-input').first().clear().type(term);
+    cy.byTestId('search-button').first().click();
+  }
+
+  public getSnackBar() {
+    return cy.get('snack-bar-container').first();
+  }
+
+  public clickPreviusPageButton() {
+    return cy.byTestId('previus-page-button').first().click();
+  }
+
+  public clickFirstPokemonItem() {
+    return cy.byTestId('pokemon-item').eq(0).click();
+  }
+
+  public clickSecondPokemonItem() {
+    return cy.byTestId('pokemon-item').eq(1).click();
+  }
+
+  public clickStartBatlePokemon() {
+    return cy.byTestId('batle-pokemon-button').first().click();
+  }
+
+  public getTitleBatleDialog() {
+    return cy.byTestId('fight-pokemon-dialog-title').first();
+  }
+}
